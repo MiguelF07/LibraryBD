59899e469157b4d72557b61fddc56959a0013494
AndreiaPp <andreia.portela@ua.pt>
2021-06-02 14:42:13 +0100

    criar projeto

---
+++ BDProjeto/BDProjeto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BDProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
