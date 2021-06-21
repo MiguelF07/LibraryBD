2f8613283eacd052101b78c68bf6b61b6ad201e2
Miguel Ferreira <miguel.r.ferreira@ua.pt>
2021-06-21 14:53:07 +0100

    changed the gos

--- atividades.sql
+++ atividades.sql
@@ -13,45 +13,45 @@ AS
 		INSERT INTO BiblioBD.atividade VALUES ('Biblioteca Municipal',@nome,@tipo,@dataAti,@horario)
 	ELSE
 		RAISERROR('ERRO: já existe uma atividade desse tipo nesse dia.',16,1);
-
--- Obter atividades
 GO
+-- Obter atividades
+
 CREATE FUNCTION BiblioBD.obterAtividades() RETURNS TABLE
 AS
 	RETURN SELECT * FROM BiblioBD.atividade WHERE dataAti>=GETDATE()
-
--- Obter membros de atividade pelo nome da atividade 
 GO
+-- Obter membros de atividade pelo nome da atividade 
+
 CREATE FUNCTION BiblioBD.membrosAtividade(@nome varchar(60)) RETURNS TABLE
 AS
 	RETURN SELECT membro,id,email,morada,BiblioBD.membro.nome,nascimento,NIF FROM BiblioBD.atividadeMembro JOIN BiblioBD.membro ON membro=id WHERE BiblioBD.atividadeMembro.nome=@nome
-
--- Eliminar atividades por nome
 GO
+-- Eliminar atividades por nome
+
 CREATE PROC BiblioBD.eliminarAtividade(@nome varchar(60))
 AS
 DELETE FROM BiblioBD.atividade WHERE nome=@nome
-
--- Obter atividades de um membro pelo id do membro
 GO
+-- Obter atividades de um membro pelo id do membro
+
 CREATE FUNCTION BiblioBD.obterAtividadesMembro(@id INT) RETURNS TABLE
 AS
 	RETURN SELECT BiblioBD.atividade.nome,tipo,dataAti,horario FROM BiblioBD.atividade JOIN BiblioBD.atividadeMembro ON BiblioBD.atividade.nome=BiblioBD.atividadeMembro.nome where dataAti>=GETDATE() and membro=@id
-
--- Obter atividades de um membro pelo id do membro e tipo de atividade
 GO
+-- Obter atividades de um membro pelo id do membro e tipo de atividade
+
 CREATE FUNCTION BiblioBD.obterAtividadesMembroTipo(@id INT,@tipo varchar(60)) RETURNS TABLE
 AS
 	RETURN SELECT BiblioBD.atividade.nome,tipo,dataAti,horario FROM BiblioBD.atividade JOIN BiblioBD.atividadeMembro ON BiblioBD.atividade.nome=BiblioBD.atividadeMembro.nome where dataAti>=GETDATE() and membro=@id and tipo=@tipo
-
--- Obter atividades por tipo
 GO
+-- Obter atividades por tipo
+
 CREATE FUNCTION BiblioBD.obterAtividadesTipo(@tipo varchar(60)) RETURNS TABLE
 AS
 	RETURN SELECT * FROM BiblioBD.atividade where dataAti>=GETDATE() and tipo=@tipo
-
--- Adiciontar membro a uma atividade
 GO
+-- Adiciontar membro a uma atividade
+
 CREATE PROCEDURE BiblioBD.adicionarMembroAtividade(@id INT,@nome varchar(60))
 AS
 	DECLARE @count INT
@@ -62,11 +62,11 @@ AS
 		INSERT INTO BiblioBD.atividadeMembro VALUES ('Biblioteca Municipal',@id,@nome)
 	ELSE
 		RAISERROR('ERRO: O membro já participa numa atividade nesse dia.',16,1);
-
--- Remover membro de uma atividade
 GO
+-- Remover membro de uma atividade
+
 CREATE PROCEDURE BiblioBD.removerMembroAtividade(@id INT,@nome varchar(60))
 AS
 	DELETE FROM BiblioBD.atividadeMembro WHERE @id=membro and @nome=nome
-	
+GO	
 
